a9f0563c9f7cdc431a585c3f0cbf02645e4d92e8
yulachi <yuafilippova@edu.hse.ru>
2023-06-08 03:58:47 +0300

    better stats calculation using nested fields

--- algo/utils/stats.py
+++ algo/utils/stats.py
@@ -1,8 +1,7 @@
 from typing import Dict
 
 from django.db.models import Avg, Max
-
-from algo.models import AlgoTask, TaskResult
+from django.db.models.query import QuerySet
 
 
 def format_stat(stat: float) -> str:
@@ -13,17 +12,14 @@ def format_stat_percent(stat: float) -> str:
     return f"{stat * 100:.0f}%"
 
 
-def get_stats() -> Dict[str, str]:
-    results = TaskResult.objects.all()
-    tasks = AlgoTask.objects.all()
-
+def get_stats(objects: QuerySet) -> Dict[str, str]:
     return {
-        "mean_cube_edge": format_stat(tasks.aggregate(Avg("a"))["a__avg"]),
-        "max_cylinder_height": format_stat(tasks.aggregate(Max("h"))["h__max"]),
+        "mean_cube_edge": format_stat(objects.aggregate(Avg("task__a"))["task__a__avg"]),
+        "max_cylinder_height": format_stat(objects.aggregate(Max("task__h"))["task__h__max"]),
         "mean_cube_fit": format_stat_percent(
-            results.aggregate(Avg("fit_in_cube"))["fit_in_cube__avg"],
+            objects.aggregate(Avg("fit_in_cube"))["fit_in_cube__avg"],
         ),
         "mean_cylinder_fit": format_stat_percent(
-            results.aggregate(Avg("fit_in_cylinder"))["fit_in_cylinder__avg"],
+            objects.aggregate(Avg("fit_in_cylinder"))["fit_in_cylinder__avg"],
         ),
     }
